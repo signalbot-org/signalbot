b3920f0ddf4a305f14cb4112ae138793d5abead8
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2026-06-03 19:37:46 +0200

    move auth stuff into dedicated file

--- src/signalbot/auth.py
+++ src/signalbot/auth.py
@@ -1,1 +1,17 @@
-init
+class Authentication(Protocol):
+    @property
+    def header(self) -> str: ...
+
+class BasicAuthentication:
+    def __init__(
+        self,
+        username: str,
+        password: str,
+    ):
+        self.username = username
+        self.password = password
+
+    @property
+    def header(self) -> str:
+        credentials = base64.b64encode(f"{self.username}:{self.password}")
+        return f"Basic {credentials}"
